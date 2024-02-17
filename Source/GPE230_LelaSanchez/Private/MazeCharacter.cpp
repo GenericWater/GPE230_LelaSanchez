97570c46f7d3943987e11f9e75408fa439162589
Lela S <lela.slenz@gmail.com>
2024-02-17 14:10:41 -0500

    Preparation for PM 1
    Prep for Project Milestone 1: Repository and Unreal Basics push

---
+++ Source/GPE230_LelaSanchez/Private/MazeCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MazeCharacter.h"
+
+// Sets default values
+AMazeCharacter::AMazeCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMazeCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMazeCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMazeCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/GPE230_LelaSanchez/Public/MazeGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MazeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GPE230_LELASANCHEZ_API AMazeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

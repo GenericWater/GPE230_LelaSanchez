6ff6e68d8540d47042d6d7d4cda8cbd94fbc9117
Lela S <lela.slenz@gmail.com>
2024-03-07 23:01:21 -0500

    Lecture 1.5: Stuck on Slide 40 in PPTX
    Errors on GENERATED_BODY() - MazeGameMode.h
    MazeGameMode.cpp - InitGameState();

--- Source/GPE230_LelaSanchez/Private/MazeGameMode.cpp
+++ Source/GPE230_LelaSanchez/Private/MazeGameMode.cpp
@@ -2,4 +2,20 @@
 
 
 #include "MazeGameMode.h"
+// Lecture 1.5:
+void AMazeGameMode::InitGameState()
+{
+	// Super refers to our header file or parent class
+	// Super::InitGameState(); will call InitGameState() in MazeGameMode.h
+	Super::InitGameState();
 
+	// If there is no override for the default pawn class...
+	if (DefaultPawnClass == ADefaultPawn::StaticClass())
+		// DefaultPawnClass is from Unreal's GameMode class > default player pawn @ spawn
+		// ADefaultPawn is the parent for the default pawn class
+	{
+		// set the default pawn to MY pawn in place of Unreal's default!
+		DefaultPawnClass = DefaultPlayerCharacter;
+		// DefaultPlayerCharacter; is the variable we made in MazeGameMode.h
+	}
+}
--- Source/GPE230_LelaSanchez/Public/MazeGameMode.h
+++ Source/GPE230_LelaSanchez/Public/MazeGameMode.h
@@ -6,6 +6,10 @@
 #include "GameFramework/GameModeBase.h"
 #include "MazeGameMode.generated.h"
 
+// Lecture 1.5 Slide 31 importation of headers
+#include "GameFramework/DefaultPawn.h" // Lets us access and change the default pawn that player spawn as
+#include "MazeCharacter.h" // Lets us access and change info from MazeCharacter C++ Class
+
 /**
  * 
  */
@@ -13,5 +17,18 @@ UCLASS()
 class GPE230_LELASANCHEZ_API AMazeGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public: // InitGameState() function to initialize our game
+	void InitGameState() override;
+
+private: // Variabel to hold our custom default character
+	// UPROPERTY modifies the variable that follows it
+	UPROPERTY(EditAnywhere, NoClear) // EditAnywhere modifies the private variable in the Blueprint
+		// NoClear prevents the variable from becoming null / empty
+
+	// TSubclassOf<AMazeCharacter> | T stands for data type 
+	// TSubclassOf<SomeType> is a variable that can only hold that data type and derived classes - inheritance
+	// Can even hold our AMazeCharacterBP!!!
+	TSubclassOf<AMazeCharacter> DefaultPlayerCharacter = AMazeCharacter::StaticClass();
+	// AMazeCharacter::StaticClass(); represents the base class @ runtime 
 };
